6cf7fad3d6374081f12c59c806fb5d09d8afb6a9
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-08-28 03:04:51 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -10,3 +10,10 @@ end
 def link
   puts "It's Dangerous To Go Alone! Take This."
 end
+
+def all_phrases
+  mario = "It's-a me, Mario!"
+  toadstool = "Thank You Mario! But Our Princess Is In Another Castle!"
+  link = "It's Dangerous To Go Alone! Take This."
+  puts (mario, toadstool, link)
+end
